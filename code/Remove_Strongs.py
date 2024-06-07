e62f00d8dfb53e705ce1bc7e7695ebdccd5491d3
Oleh Onyshchenko <110289150+seesmof@users.noreply.github.com>
2024-06-07 20:26:05 +0300

    feat: Hallelujah thank You Jesus Christ our Holy Lord GOD Almighty ✝️💓🙏

--- code/Remove_Strongs.py
+++ code/Remove_Strongs.py
@@ -1,11 +1,17 @@
 import os
 import re
+from rich.console import Console
+from rich.traceback import install
+
+install()
+console = Console()
 
 
 def Remove_Strongs(Directory):
     for Root, Dirs, Files in os.walk(Directory):
         for File_Name in Files:
-            print(File_Name)
+            if "usfm" not in File_Name:
+                continue
             File_Path = os.path.join(Root, File_Name)
 
             with open(File_Path, "r", encoding="utf-8") as Current_File:
@@ -28,4 +34,12 @@ Bibles_Directory = os.path.join(Current_Directory, "..", "Bibles")
 for Root, Dirs, Files in os.walk(Bibles_Directory):
     for Directory in Dirs:
         Directory_Path = os.path.join(Root, Directory)
-        Remove_Strongs(Directory_Path)
+
+        First_USFM_File = os.path.join(Directory_Path, os.listdir(Directory_Path)[0])
+        with open(First_USFM_File, "r", encoding="utf-8") as Current_File:
+            File_Content = Current_File.read()
+        if "|strong=" not in File_Content:
+            continue
+
+        with console.status(f"Working on {Directory}..."):
+            Remove_Strongs(Directory_Path)
